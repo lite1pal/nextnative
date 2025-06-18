d6de41305236bdcf6da26ad0d24be0f4fefdcd0c
Denis Tarasenko <deniskatarasenko6@gmail.com>
2025-06-18 20:11:28 +0300

    feat(home): Implement lazy loading for feature section videos
    - Implemented lazy loading for videos in the feature section using Intersection Observer.
    - Added a loading spinner while the video is loading.
    - Updated the FeatureSection layout to use xl:grid-cols-2 for larger screens.
    - Added placeholder image to display before video loads.
    - Modified Container component to remove md:px-0 and replace with xl:px-0.

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,6 +1,6 @@
 function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="mx-auto w-full max-w-[962px] xl:max-w-[1260px] px-4 md:px-0">
+    <div className="mx-auto w-full max-w-[962px] xl:max-w-[1260px] px-4 xl:px-0">
       {children}
     </div>
   );

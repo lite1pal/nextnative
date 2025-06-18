4736e137230062696fbe04ef6dcd9dd12188d675
Denis Tarasenko <deniskatarasenko6@gmail.com>
2025-06-18 15:36:29 +0300

    feat(next): Enable CORS for API routes
    - Add headers configuration to allow cross-origin requests to /api routes
    - Set Access-Control-Allow-Origin to allow all origins
    - Set Access-Control-Allow-Methods to allow GET
    - Set Access-Control-Allow-Headers to allow X-CSRF-Token, Content-Type, and Authorization

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,27 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-CSRF-Token, Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;

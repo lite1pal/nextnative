e8b3a50644fcd5e270b6cda6aa7ff8ef822a5674
Denis Tarasenko <deniskatarasenko6@gmail.com>
2025-07-01 10:37:27 +0300

    fix(pricing): Update copy in PricingSection
    - Change "2 production-ready template apps" to "2 production-ready apps included" in Growth plan features.
    - Remove "1 production-ready template app" from Starter plan features.

--- components/PricingSection.tsx
+++ components/PricingSection.tsx
@@ -43,7 +43,7 @@ const pricingFeatures: PricingFeature[] = [
         // href="/components"
         className="text-primary group transition-colors relative"
       >
-        2 production-ready template apps
+        2 production-ready apps included
         <span className="h-0.5 group-hover:w-full w-0 transition-all duration-300 absolute bottom-0 left-0 bg-primary rounded-full"></span>
       </div>
     ),
@@ -60,20 +60,20 @@ const pricingFeaturesStarter: PricingFeature[] = [
   { text: "Push notifications" },
   { text: "In-App purchases" },
   { text: "Database" },
-  {
-    text: (
-      <div
-        onClick={() => {
-          trackEvent("PricingSection_Beautiful_UI_components_clicked");
-        }}
-        // href="/components"
-        className="text-primary group transition-colors relative"
-      >
-        1 production-ready template app
-        <span className="h-0.5 group-hover:w-full w-0 transition-all duration-300 absolute bottom-0 left-0 bg-primary rounded-full"></span>
-      </div>
-    ),
-  },
+  // {
+  //   text: (
+  //     <div
+  //       onClick={() => {
+  //         trackEvent("PricingSection_Beautiful_UI_components_clicked");
+  //       }}
+  //       // href="/components"
+  //       className="text-primary group transition-colors relative"
+  //     >
+  //       1 production-ready template app
+  //       <span className="h-0.5 group-hover:w-full w-0 transition-all duration-300 absolute bottom-0 left-0 bg-primary rounded-full"></span>
+  //     </div>
+  //   ),
+  // },
   { text: "Lifetime updates" },
 ];
 

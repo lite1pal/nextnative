23a94ac42789ed03a65bcc0bab1f0697bc30a722
Denis <deniskatarasenko6@gmail.com>
2025-12-22 07:57:10 +0100

    fix: replace HighlightedSpan2 with original one for Subheadings

--- components/Subheading.tsx
+++ components/Subheading.tsx
@@ -1,4 +1,4 @@
-import HighlightedSpan2 from "./HighlightedSpan2";
+import HighlightedSpan from "./HighlightedSpan";
 import HorizontalLine from "./HorizontalLine";
 import { cn } from "@/lib/utils";
 
@@ -18,7 +18,7 @@ function Subheading({
       {withHorizontalLine && <HorizontalLine className="w-[94px]" />}
       <h2 className="text-[40px] leading-[52px] font-[500] md:text-[54px] md:leading-[71px]">
         {heading1} <br />
-        <HighlightedSpan2>{heading2}</HighlightedSpan2>
+        <HighlightedSpan>{heading2}</HighlightedSpan>
       </h2>
     </div>
   );

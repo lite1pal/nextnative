5c170007e56135163565154eee4a64d6fda39ab7
Denis <deniskatarasenko6@gmail.com>
2026-01-16 15:00:28 +0100

    fix: correct discount text in UrgencyText component

--- components/UrgencyText.tsx
+++ components/UrgencyText.tsx
@@ -6,7 +6,7 @@ export default function UrgencyText() {
   const { customersCount, discountLimit } = usePurchaseStats();
   return (
     <p className="font-medium text-gray-500 sm:text-xl">
-      <span className="text-red-500">🎁 50 off, </span> for the first{" "}
+      <span className="text-red-500">🎁 50% off, </span> for the first{" "}
       {discountLimit} customers ({discountLimit - customersCount} left)
     </p>
   );

788e850d2fc365b40f4071a5b8f9b0c8032cc066
Denis <deniskatarasenko6@gmail.com>
2026-05-18 09:50:56 +0200

    feat: add secondary Book Call button and update CTA components

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,11 +1,13 @@
 import { cn } from "@/lib/cn";
 import CTAButton from "./CTAButton";
+import CTAButtonSecondary from "./CTASecondary";
 
 export default function CTA({ className }: { className?: string }) {
   return (
     <div className={cn("flex flex-col gap-4 xl:items-center", className)}>
       <div className="flex flex-col gap-3 md:flex-row">
         <CTAButton />
+        <CTAButtonSecondary />
       </div>
     </div>
   );

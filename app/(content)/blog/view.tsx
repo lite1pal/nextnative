8fe7ed700f50c26db4e0f79a97304778d3e17075
Denis <deniskatarasenko6@gmail.com>
2026-05-18 11:52:09 +0200

    refactor: comment out revalidation and set dynamic rendering to force-dynamic in blog page

--- app/(content)/blog/view.tsx
+++ app/(content)/blog/view.tsx
@@ -2,8 +2,6 @@ import { Suspense } from "react";
 import BlogHeading from "./blog-heading";
 import PostsGrid from "./posts-grid";
 
-export const dynamic = "force-dynamic";
-
 export default function BlogViewPage() {
   return (
     <div className="flex flex-col items-center gap-5">

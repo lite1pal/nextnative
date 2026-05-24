1e25f7a0f53a514597a59219220fe9662bec4213
Denis <deniskatarasenko6@gmail.com>
2026-05-24 19:02:32 +0200

    feat: uncomment security headers in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -70,18 +70,18 @@ const nextConfig: NextConfig = {
           //   key: "X-Frame-Options",
           //   value: "DENY",
           // },
-          // {
-          //   key: "X-Content-Type-Options",
-          //   value: "nosniff",
-          // },
-          // {
-          //   key: "Referrer-Policy",
-          //   value: "strict-origin-when-cross-origin",
-          // },
-          // {
-          //   key: "Permissions-Policy",
-          //   value: "camera=(), microphone=(), geolocation=()",
-          // },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=()",
+          },
           // {
           //   key: "Content-Security-Policy",
           //   value:
